3d0bb7bfaf713f8b2f442fbe3088d9849269588b
emjanssen <107078132+emjanssen@users.noreply.github.com>
2025-07-15 16:08:25 -0400

    all tests pass, all comments added

--- src/assignment2/Account.java
+++ src/assignment2/Account.java
@@ -15,10 +15,12 @@ public class Account {
 
     // no arguments
     public Account() {
+        // default values
         String accountNameDefault = "DEF-";
         int accountNumberDefault = 11111;
         double accountBalanceDefault = 11.11;
 
+        // set to default when we have no arguments
         this.accountNumber = accountNumberDefault;
         this.accountName = accountNameDefault;
         this.accountBalance = accountBalanceDefault;
@@ -26,14 +28,17 @@ public class Account {
 
     // three arguments
     public Account(String accountName, int accountNumber, double accountBalance) {
-
-        // if functions return true, assign new values
-        // if functions return false, assign default value
-
+        // default values
         String accountNameDefault = "DEF-";
         int accountNumberDefault = 11111;
         double accountBalanceDefault = 11.11;
 
+        // if functions return true, assign new values from arguments
+        this.accountName = accountName;
+        this.accountNumber = accountNumber;
+        this.accountBalance = accountBalance;
+
+        // if functions return false, assign default values
         if (accountName == null || !setAccountName(accountName)) {
             this.accountName = accountNameDefault;
         }
@@ -49,33 +54,45 @@ public class Account {
 
     // -- Helper Methods -- //
 
+    // function purpose: validate accountName input
+    // return false is any input is invalid; return true is all input is valid
     public static boolean validateAccountName(String accountName) {
+        // permitted characters via regex pattern: a-z, a-Z, hyphen, whitespace; zero or more times
         String accountNameRegex = "^[a-zA-Z\\- ']*$";
+        // compile into permitted pattern
         Pattern permitted = Pattern.compile(accountNameRegex);
+        // additional constrains on whitespace and single quote characters, as each can only appear up to once
+        // will use count values to count char instances
         int whitespaceCount = 0;
         int singleQuoteCount = 0;
 
+        // is the string is less than four characters
         if (accountName.length() < 4) {
             System.out.println("The account name must be at least four characters.");
             System.out.println("Returning false...");
             return false;
         }
 
+        // if the chars don't equal the permitted pattern
         if (!permitted.matcher(accountName).matches()) {
             System.out.println("Your account name contains invalid characters.");
             System.out.println("Returning false...");
             return false;
         }
 
+        // conspiile pattern to search for whitespace chars; match that against accountName
         Matcher whitespaceMatcher = Pattern.compile("\\s").matcher(accountName);
+        // while we have whitespace chars to find, run the while loop, and increment count on each loop
         while (whitespaceMatcher.find())
             whitespaceCount++;
+        // if the whitespaceCount ends up being more than one, input is invalid
         if (whitespaceCount > 1) {
             System.out.println("You have included too many whitespace chars.");
             System.out.println("Returning false...");
             return false;
         }
 
+        // same process for single quotes as with white space characters
         Matcher quoteMatcher = Pattern.compile("'").matcher(accountName);
         while (quoteMatcher.find())
             singleQuoteCount++;
@@ -89,16 +106,21 @@ public class Account {
         return true;
     }
 
+    // function purpose: validate account number
+    // return false is any input is invalid; return true is all input is valid
     public static boolean validateAccountNumber(int accountNumber) {
 
+        // cast int to string for purpose of checking against a regex pattern
         String accountNumberAsString = Integer.toString(accountNumber);
 
+        // required pattern: a number between 1 to 9 (so we have no leading zeroes), and then between 4-8 of numbers from 0-9; gives us 5-9 chars total
         if (!accountNumberAsString.matches("^[1-9][0-9]{4,8}$")) {
             System.out.println("Input has too many or too few digits, or has leading zeros.");
             System.out.println("Returning false...");
             return false;
         }
 
+        // if input is a negative
         if (accountNumber < 1) {
             System.out.println("Input is not a positive number.");
             System.out.println("Returning false...");
@@ -109,9 +131,29 @@ public class Account {
         return true;
     }
 
+    // validate accountBalance input
+    // return false is any input is invalid; return true is all input is valid
     public static boolean validateAccountBalance(double accountBalance) {
+        // cast accountBalance to string for purpose of comparing against regex pattern
         String valueAsString = Double.toString(accountBalance);
 
+        /* regex pattern:
+        ^               : start of string
+        -?              : optional minus sign
+            (               : start of group
+            \\d+            : one or more digits
+            (\.\d{1,2})?    : optional - decimal followed by up to two digits
+            |               : OR
+            \\.\d{1,2}      : decimal followed by up to two digits (no leading digits)
+            )               : end of group
+        $               : end of string
+
+         matches either:
+         1. whole number, with optional decimal
+         2. number starting with decimal
+
+         */
+        // if balance doesn't match pattern
         if (!valueAsString.matches("^-?(\\d+(\\.\\d{1,2})?|\\.\\d{1,2})$")) {
             System.out.println("Input is invalid.");
             System.out.println("Returning false...");
@@ -128,6 +170,7 @@ public class Account {
         return this.accountName;
     }
 
+    // function purpose: validate account name, and either update value, or return false
     public boolean setAccountName(String accountName) {
 
         if (validateAccountName(accountName)) {
@@ -144,6 +187,7 @@ public class Account {
         return this.accountNumber;
     }
 
+    // function purpose: validate account number, and either update value, or return false
     public boolean setAccountNumber(int accountNumber) {
 
         if (validateAccountNumber(accountNumber)) {
@@ -160,6 +204,7 @@ public class Account {
         return this.accountBalance;
     }
 
+    // function purpose: validate account balance, and either update value, or return false
     public boolean setAccountBalance(double accountBalance) {
 
         if (validateAccountBalance(accountBalance)) {
@@ -187,6 +232,7 @@ public class Account {
 
         // if the object we're comparing isn't an Account object
         // return false to prevent ClassCastException exception
+        // can pass in any kind of object to equals(); need to make sure we're actually comparing two Account objects
         if (!(obj instanceof Account)) {
             return false;
         }
@@ -205,6 +251,7 @@ public class Account {
 
     }
 
+    // override toString() to print out a summary of the object instance
     @Override
     public String toString() {
         return "Account Name: " + accountName + "\nAccount Number: " + accountNumber + " \nBalance: $" + String.format("%.2f", accountBalance);
--- src/assignment2/Bank.java
+++ src/assignment2/Bank.java
@@ -11,31 +11,37 @@ public class Bank {
 
     // - - - Variables - - - //
 
+    // BranchLocations objects with list of branch locations
     public static enum BranchLocations {
         TORONTO, VANCOUVER, MONTREAL, CALGARY, HALIFAX, DEFAULT
     }
 
     private String bankName;
     private BranchLocations branchLocation;
+    // array list to track accounts
     private List<Account> accounts = new ArrayList<>();
 
     // - - - Constructors - - - //
 
     // no arguments
     public Bank() {
+        // default values
         String defaultBankName = "BANK&DEF123";
         BranchLocations defaultBranch = BranchLocations.DEFAULT;
 
+        // set to default when we have no arguments
         this.bankName = defaultBankName;
         this.branchLocation = defaultBranch;
     }
 
-    // two arguments: bankName,
+    // two arguments: bankName, branchLocation String object
     public Bank(String bankName, String branchLocation) {
 
+        // default values
         String defaultBankName = "BANK&DEF123";
         BranchLocations defaultBranch = BranchLocations.DEFAULT;
 
+        // if functions return false, assign default values
         if (bankName == null || !setBankName(bankName)) {
             this.bankName = defaultBankName;
         }
@@ -45,13 +51,14 @@ public class Bank {
         }
     }
 
-    //two arguments: bankName,
+    //two arguments: bankName, branchLocation BranchLocations object
     public Bank(String bankName, BranchLocations branchLocation) {
 
+        // default values
         String defaultBankName = "BANK&DEF123";
         BranchLocations defaultBranch = BranchLocations.DEFAULT;
 
-
+        // if functions return false, assign default values
         if (!setBankName(bankName)) {
             this.bankName = defaultBankName;
         }
@@ -62,33 +69,44 @@ public class Bank {
 
     // - - - Helper Methods - - - //
 
+    // function purpose: validation bankName input, return false if any input is invalid, return true if all inputs are valid
     public static boolean validateBankName(String bankName) {
+        // permitted characters via regex pattern: a-z, A-Z, &, 0-9, whitespace; zero or more times
         String bankNameRegex = "^[a-zA-Z&0-9 ]*$";
+        // compile into permitted pattern
         Pattern permittedBankNamePattern = Pattern.compile(bankNameRegex);
+        // additional constrains on whitespace and digits characters
+        // will use count values to count char instances
         int whitespaceCount = 0;
         int digitCount = 0;
+        // compile matcher patterns to search for (whitespace, digit)
         Matcher whitespaceMatcher = Pattern.compile(" ").matcher(bankName);
         Matcher digitMatcher = Pattern.compile("\\d").matcher(bankName);
 
+        // if bank name doesn't have enough chars
         if (bankName.length() < 8) {
             System.out.println("The bank name must be at least eight characters.");
             System.out.println("Returning false...");
             return false;
         }
 
+        // if bank name doesn't have the chars permitted via our regex pattern
         if (!permittedBankNamePattern.matcher(bankName).matches()) {
             System.out.println("The bank name contains invalid characters.");
             System.out.println("Returning false...");
             return false;
         }
 
+        // while we have whitespace chars to find, run the while loop, and increment count on each loop
         while (whitespaceMatcher.find()) whitespaceCount++;
+        // if the whitespaceCount ends up being more than one, input is invalid
         if (whitespaceCount > 1) {
             System.out.println("You have included too many whitespace characters.");
             System.out.println("Returning false...");
             return false;
         }
 
+        // same process for digits (except digit count needs to be no more than 3)
         while (digitMatcher.find()) digitCount++;
         if (digitCount > 3) {
             System.out.println("You have included too many digits.");
@@ -106,6 +124,7 @@ public class Bank {
         return bankName;
     }
 
+    // function purpose: validate bank name; return false is any input is invalid; return true is all input is valid
     public boolean setBankName(String bankName) {
 
         if (validateBankName(bankName)) {
@@ -118,6 +137,7 @@ public class Bank {
 
     // - - - Methods: Branch Location - - - //
 
+    // function purpose: validate branch location input, see if it matches list of enums
     public boolean setBranchLocation(String branchLocation) {
 
         if (branchLocation == null || branchLocation.trim().isEmpty()) {
@@ -150,6 +170,7 @@ public class Bank {
         return this.branchLocation.toString();
     }
 
+    // functinn purpose: validate whether BranchLocations object is null; update if it isn't
     public boolean setBranchLocation(BranchLocations branchLocation) {
         if (branchLocation == null) {
             System.out.println("Branch location is null. Not updating branch location. Returning false...");
@@ -163,20 +184,28 @@ public class Bank {
 
     // - - -  Methods: Accounts - - - //
 
+    // validate accountNumber input, iterater over accounts list, return match if it's found, create new object if match isn't found
     public Account getAccountByNumber(int accountNumber) {
 
+        // once input has been validated via helper function, iterate over accounts list
         if (validateAccountNumber(accountNumber)) {
+            // use length (i.e. size() ) of accounts list to know how many loops to iterate
             for (int i = 0; i < accounts.size(); i++) {
+                // if current iteration value of object's getAccountNumber() outcome matches the input passed into thisd function
                 if (accounts.get(i).getAccountNumber() == accountNumber) {
+                    // then we have found a matching account; we return that account
                     System.out.println("We have found your account.");
                     return accounts.get(i);
                 }
             }
         }
+
+        // if no match found, we return a new object using the default (no arguments) constructor
         System.out.println("Account not found. Returning default constructor");
         return new Account();
     }
 
+    // same process as getAccountByNumber(), just with entire object passed in, instead of just the account number
     public boolean addAccount(Account account) {
 
         for (int i = 0; i < accounts.size(); i++) {
@@ -192,6 +221,7 @@ public class Bank {
         return true;
     }
 
+    // validate inputs, iterate over accounts list, return match if it's found, create new object if match isn't found
     public boolean addAccount(String accountName, int accountNumber, double accountBalance) {
 
         if (validateAccountName(accountName) && validateAccountNumber(accountNumber) && validateAccountBalance(accountBalance)) {
@@ -209,6 +239,7 @@ public class Bank {
         return true;
     }
 
+    // validate input, iterate over accounts list, return match if it's found, create new object if match isn't found
     public Account viewAccount(int accountNumber) {
 
         if (validateAccountNumber(accountNumber)) {
@@ -224,15 +255,20 @@ public class Bank {
         return new Account();
     }
 
+    // find account using index value
     public Account viewAccount(byte index) {
+        // confirm that index argument is not less than 0, and isn't greater than our array length
         if (index < 0 || index >= accounts.size()) {
+            // if index is out of range, create a new object
             System.out.println("Invalid index: " + index + ". Returning default constructor...");
             return new Account();
         }
 
+        // if index is valid, return account at that index
         return accounts.get(index);
     }
 
+    // validate inputs, validate the index is valid, iterate over accounts to search for a match,then update relevant arguments using setters
     public boolean modifyAccount(int accountNumber, String accountName) {
 
         if (validateAccountName(accountName) && validateAccountNumber(accountNumber)) {
@@ -247,12 +283,14 @@ public class Bank {
                 }
             }
         }
+        // return false if account not found
         System.out.println("Account not found. Returning false...");
         return false;
     }
 
     public boolean modifyAccount(int accountNumber, double accountBalance) {
 
+        // validate inputs, validate the index is valid, iterate over accounts to search for a match,then update relevant arguments using setters
         if (validateAccountBalance(accountBalance) && validateAccountBalance(accountBalance)) {
             for (int i = 0; i < accounts.size(); i++) {
                 Account existingAccount = accounts.get(i);
@@ -265,11 +303,12 @@ public class Bank {
                 }
             }
         }
-
+        // return false if account not found
         System.out.println("Account not found. Returning false...");
         return false;
     }
 
+    // validate inputs, validate the index is valid, iterate over accounts to search for a match,then update relevant arguments using setters
     public boolean modifyAccount(int accountNumber, String accountName, double accountBalance) {
 
         if (validateAccountName(accountName) && validateAccountNumber(accountNumber) && validateAccountBalance(accountBalance)) {
@@ -286,16 +325,18 @@ public class Bank {
                 }
             }
         }
-
+        // return false if account not found
         System.out.println("Account not found. Returning false...");
         return false;
     }
 
     // - - - Methods: index - - - //
 
+    // validate input, validate index, then either return false or update the relevant object values
     public boolean modifyAccount(byte index, String accountName) {
 
         if (validateAccountName(accountName)) {
+            // if index is out of range, return false
             if (index < 0 || index >= accounts.size()) {
                 System.out.println("Invalid index: " + index + ". Returning false...");
                 return false;
@@ -303,6 +344,7 @@ public class Bank {
             }
         }
 
+        // if all inputs are valid, get the account at the index passed in, use setters to update relevant values
         Account selectedAccount = accounts.get(index);
         System.out.println("Account " + selectedAccount.getAccountNumber() + " found at index " + index + ".");
         selectedAccount.setAccountName(accountName);
@@ -310,15 +352,18 @@ public class Bank {
         return true;
     }
 
+    // validate input, validate index, then either return false or update the relevant object values
     public boolean modifyAccount(byte index, double accountBalance) {
 
         if (validateAccountBalance(accountBalance)) {
+            // if index is out of range, return false
             if (index < 0 || index >= accounts.size()) {
                 System.out.println("Invalid index: " + index + ". Returning false...");
                 return false;
             }
         }
 
+        // if all inputs are valid, get the account at the index passed in, use setters to update relevant value
         Account selectedAccount = accounts.get(index);
         System.out.println("Account " + selectedAccount.getAccountNumber() + " found at index " + index + ".");
         selectedAccount.setAccountBalance(accountBalance);
@@ -326,15 +371,18 @@ public class Bank {
         return true;
     }
 
+    // validate input, validate index, then either return false or update the relevant object values
     public boolean modifyAccount(byte index, String accountName, double accountBalance) {
 
         if (validateAccountName(accountName) && validateAccountBalance(accountBalance)) {
+            // if index is out of range, return false
             if (index < 0 || index >= accounts.size()) {
                 System.out.println("Invalid index: " + index + ". Returning false...");
                 return false;
             }
         }
 
+        // if all inputs are valid, get the account at the index passed in, use setters to update relevant values
         Account selectedAccount = accounts.get(index);
         System.out.println("Account " + selectedAccount.getAccountNumber() + " found at index " + index + ".");
         selectedAccount.setAccountName(accountName);
@@ -346,6 +394,7 @@ public class Bank {
 
     public boolean deleteAccount(int accountNumber) {
 
+        // validate inputs then search for account object; if match found, remove it from accounts list
         if (validateAccountNumber(accountNumber)) {
             for (int i = 0; i < accounts.size(); i++) {
                 Account existingAccount = accounts.get(i);
@@ -357,10 +406,12 @@ public class Bank {
             }
         }
 
+        // return false if not match found
         System.out.println("Account not found. Returning false...");
         return false;
     }
 
+    // validate index; if valid, search for account object; if match found, remove object from accounts list
     public boolean deleteAccount(byte index) {
         if (index < 0 || index >= accounts.size()) {
             System.out.println("Invalid index: " + index + ". Returning false...");
