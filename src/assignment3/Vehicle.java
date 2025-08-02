869399310c25b82d333aca1b5971061d12e91889
emjanssen <107078132+emjanssen@users.noreply.github.com>
2025-08-02 03:20:01 -0400

    Update Vehicle.java

--- src/assignment3/Vehicle.java
+++ src/assignment3/Vehicle.java
@@ -1,4 +1,50 @@
 package assignment3;
 
 public class Vehicle {
+
+    /* Private Variables */
+
+    private String color;
+    private int[] numberOfDoors;
+    private boolean isGasPowered;
+
+    /* Getters and Setters */
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int[] getNumberOfDoors() {
+        return numberOfDoors;
+    }
+
+    public void setNumberOfDoors(int[] numberOfDoors) {
+        this.numberOfDoors = numberOfDoors;
+    }
+
+    public boolean isGasPowered() {
+        return isGasPowered;
+    }
+
+    public void setGasPowered(boolean gasPowered) {
+        isGasPowered = gasPowered;
+    }
+
+    /* Constructors */
+
+    public Vehicle(String color, int[] numberOfDoors, boolean isGasPowered) {
+        this.color = color;
+        this.numberOfDoors = numberOfDoors;
+        this.isGasPowered = isGasPowered;
+    }
+
+
+
+
+
+
 }
