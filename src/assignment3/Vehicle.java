dd22362f946543d61cd5c18b10279c7ca5358e60
emjanssen <107078132+emjanssen@users.noreply.github.com>
2025-08-02 16:06:55 -0400

    first pass at Vehicle.java complete

--- src/assignment3/Vehicle.java
+++ src/assignment3/Vehicle.java
@@ -1,50 +1,139 @@
 package assignment3;
 
-public class Vehicle {
+public class Vehicle { // - Done
 
-    /* Private Variables */
+    // 1) Create 3 instance variables with unique data types and unique accessibility levels to represent the following attributes of a vehicle
+    // - - - Private Variables - - - //
 
     private String color;
-    private int[] numberOfDoors;
+    private int numberOfDoors;
     private boolean isGasPowered;
 
-    /* Getters and Setters */
+    // 3) Create the necessary getters and setters for this class
+    // - - - Getters and Setters - - - //
 
     public String getColor() {
         return color;
     }
 
     public void setColor(String color) {
+
+        // validation login //
         this.color = color;
     }
 
-    public int[] getNumberOfDoors() {
+    public int getNumberOfDoors() {
         return numberOfDoors;
     }
 
-    public void setNumberOfDoors(int[] numberOfDoors) {
+    public void setNumberOfDoors(int numberOfDoors) {
+
+        // validation login //
         this.numberOfDoors = numberOfDoors;
     }
 
-    public boolean isGasPowered() {
+    public boolean getIsGasPowered() {
         return isGasPowered;
     }
 
-    public void setGasPowered(boolean gasPowered) {
-        isGasPowered = gasPowered;
+    public void setIsGasPowered(boolean isGasPowered) {
+
+        // validation login //
+        this.isGasPowered = isGasPowered;
     }
 
-    /* Constructors */
+    // 2) Create 4 constructors with parameters + the default constructor
+    // - - - Constructors - - - //
 
-    public Vehicle(String color, int[] numberOfDoors, boolean isGasPowered) {
+    // no arguments; all default values
+    public Vehicle() {
+
+        String defaultColor = "black";
+        int defaultNumberOfDoors = 4;
+        boolean defaultIsGasPowered = true;
+
+        this.color = defaultColor;
+        this.numberOfDoors = defaultNumberOfDoors;
+        this.isGasPowered = defaultIsGasPowered;
+    }
+
+    // colour - call default constructor, which assigns default values; override with user input for colour
+    public Vehicle(String color) {
+        this();
+        this.color = color;
+    }
+
+    // number of doors - call default constructor, which assigns default values; override with user input for number of doors
+    public Vehicle(int numberOfDoors) {
+        this();
+        this.numberOfDoors = numberOfDoors;
+    }
+
+    // gas-powered - call default constructor, which assigns default values; override with user input for isGasPowered
+    public Vehicle(boolean isGasPowered) {
+        this();
+        this.isGasPowered = isGasPowered;
+    }
+
+    // all arguments - call default constructor, which assigns default values; override with user input for all parameters
+    public Vehicle(String color, int numberOfDoors, boolean isGasPowered) {
         this.color = color;
         this.numberOfDoors = numberOfDoors;
         this.isGasPowered = isGasPowered;
     }
 
+    // - - - Methods - - - //
 
+    //6) Create a method named isEcoFriendly. It has no parameters. It determines whether the Vehicle two-doored and is not gas powered.
+    // can call this on a Vehicle object and see if vehicle is eco-friendly as per method declaration
+    public boolean isEcoFriendly() {
 
+        if ((getNumberOfDoors() != 2) || (getIsGasPowered())) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
 
+    // - - -  Overrides - - - //
 
+    // 4) Override (not Overload) the equals() method so it evaluates two Vehicle objects and returns true if and only if (iff) the two Vehicle objects are equal is value
+    // overriding the equals() function so we can compare specific Object types
+    @Override
+    public boolean equals(Object object) {
+        if (this == object)
+        // if the objects are the same (i.e. both references point to the same object in memory), return true and exit function
+        {
+            return true;
+        }
 
+        // if the object is null, return false and exit function
+        if (object == null) {
+            return false;
+        }
+
+        // if the object we're comparing isn't a Vehicle object
+        // return false to prevent ClassCastException exception
+        // can pass in any kind of object to equals(); we need to make sure we're actually comparing two Vehicle objects
+        if (getClass() != object.getClass()) {
+            return false;
+        }
+
+        // if we haven't exited already due to true and false returns above
+        // equals(Object object) passes in any type of object; code doesn't know we want a Vehicle object
+        // we need to cast the object being passed in to Vehicle so code can compare Vehicle objects properly
+        Vehicle otherVehicle = (Vehicle) object;
+
+        // return a comparison for all fields in the constructor
+        // we know they're not the exact same object, but they can still have all the same field attributes
+        return this.color.equals(otherVehicle.color) && this.numberOfDoors == otherVehicle.numberOfDoors && this.isGasPowered == otherVehicle.isGasPowered;
+    }
+
+    // 5) Override the toString method to summarize all instance variables of the class
+    // override toString() to print out a summary of the Vehicle instance
+    @Override
+    public String toString() {
+        return "Vehicle color: " + color + "\nNumber of doors: " + numberOfDoors + " \nWhether the vehicle is gas-powered: " + isGasPowered;
+    }
 }
