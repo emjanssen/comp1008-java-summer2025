6c5928dac3c9c1a4a992db89feacc3d7f96d7baa
emjanssen <107078132+emjanssen@users.noreply.github.com>
2025-08-02 21:56:27 -0400

    button.java complete

--- src/assignment3/Button.java
+++ src/assignment3/Button.java
@@ -1,4 +1,21 @@
 package assignment3;
 
+// 7) Create an interface named Button (1 mark)
 public interface Button {
+
+    // - - - Public Variables - - - //
+
+    // 8) Code two constant values (2 marks)
+    // enter key as the default button press
+    // ASCII/Unicode 10 = enter key
+    public static final int DEFAULT_KEYPRESS = 10;
+    // 0 as the default words per minute value
+    public static final int DEFAULT_WORDS_PER_MINUTE = 0;
+
+    // - - - Methods - - - //
+
+    // 9) Code two methods (2 marks)
+    // methods will be implemented in Computer class
+    public boolean listenForButtonPress();
+    public int countWordsPerMinute();
 }
