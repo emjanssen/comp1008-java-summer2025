4ad443a6b42adce68d3e0fd34f2c667002f580f8
emjanssen <107078132+emjanssen@users.noreply.github.com>
2025-08-02 23:26:32 -0400

    initial computer.java; still need to edit methods

--- src/assignment3/Computer.java
+++ src/assignment3/Computer.java
@@ -1,4 +1,35 @@
 package assignment3;
 
-public class Computer {
+/*
+1) Create concrete class named Computer
+2) Base this class on the abstract Keyboard class (1 mark)
+3) This class is to implement the Button interface (1 mark)
+ */
+public class Computer extends Keyboard implements Button {
+
+    // a) Complete the interface method bodies (2 x 2 marks)
+
+    // - - - Abstract Methods from Button Interface - - - //
+
+    @Override
+    public boolean listenForButtonPress() {
+        return false;
+    }
+
+    @Override
+    public int countWordsPerMinute() {
+        return 0;
+    }
+
+    // - - - Abstract Methods from Keyboard Class - - - //
+
+    @Override
+    public boolean isWireless() {
+        return false;
+    }
+
+    @Override
+    public boolean hasRGBLighting() {
+        return false;
+    }
 }
